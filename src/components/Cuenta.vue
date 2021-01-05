e211dfa7d8faf09c31e6be9dbdce0c6b0a7f8f7b
Jonathan Pumares <jepc1491@gmail.com>
2021-01-05 00:12:06 -0600

    Práctica de un banco dinámico del curso de VueJS utilizando componentes.

---
+++ src/components/Cuenta.vue
@@ -0,0 +1,53 @@
+<template>
+  <h2>Tipo de cuenta: {{ cuenta }}</h2>
+  <h2>Saldo: {{ saldo }}</h2>
+  <h2>Cuenta: {{ estado ? 'Activa' : 'Desactivada' }}</h2>
+  <div v-for="(servicio, index) in servicios" :key="index">
+    {{ index + 1 }} - {{ servicio }}
+  </div>
+  <AccionSaldo 
+    texto="Aumentar Saldo"
+    @accion="aumentar"/>
+  <AccionSaldo 
+    texto="Disminuir Saldo"
+    @accion="disminuir"
+    :desactivar="desactivar"/>
+</template>
+
+<script>
+import AccionSaldo from './AccionSaldo.vue'
+
+export default {
+  components: {
+    AccionSaldo
+  },
+  data() {
+    return {
+      saldo: 1000,
+      cuenta: 'Visa',
+      estado: true,
+      servicios: ['giro', 'abono', 'transferencia'],
+      desactivar: false
+    }
+  },
+  methods: {
+    aumentar() {
+      this.saldo = this.saldo + 100
+      this.desactivar = false
+    },
+    disminuir() {
+      if (this.saldo === 0) {
+        alert('Saldo agotado')
+        this.desactivar = true
+      }
+      else {
+        this.saldo = this.saldo - 100
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
